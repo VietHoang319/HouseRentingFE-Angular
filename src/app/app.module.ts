cf3425f1e87c5fcbd06c938df9c3f11ccd801278
VietHoang319 <arishoang319@gmail.com>
2022-07-23 14:00:27 +0700

    23/7 - Hoàng - done login (chưa liên kết trang)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/blocks/navbar/navbar.component';
+import { LoginComponent } from './component/pages/login/login.component';
+import { RegisterComponent } from './component/pages/register/register.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
