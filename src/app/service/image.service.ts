83b2aebf4a5d7f346ea67335fbaee70076239e9e
VietHoang319 <arishoang319@gmail.com>
2022-07-25 16:29:32 +0700

    25/07 - Hoàng - thêm ảnh vào detail

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
 
@@ -14,5 +14,11 @@ export class ImageService {
 
   getFirstImageByHouse(id: number) : Observable<any> {
     return this.httpClient.get(API_URL + `/${id}`)
+  }
+
+  getAllByHouseId(id: number): Observable<any> {
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append("id", id);
+    return this.httpClient.get(API_URL + "/find-all-by-house-id", {params: queryParams})
   }
 }
